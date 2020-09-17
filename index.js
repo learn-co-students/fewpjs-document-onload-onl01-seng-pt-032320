103d4d9e34b372e7be8428547443828a69b7f34a
curtis <laurencechadwell@gmail.com>
2020-09-17 10:07:58 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+
+let text = document.getElementById('text');
+document.addEventListener('DOMContentLoaded' , pageFullyLoaded, false)
+
+function pageFullyLoaded(e){
+    text.innerHTML = 'This is really cool!'
+}
