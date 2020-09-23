f33bc0ce6859ec21330c6a790124c60a0a25123e
megfly <mflynn333@gmail.com>
2020-09-23 18:18:58 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function(){
+    document.getElementById("text").innerHTML = "This is really cool!";
+  });
+   
+  console.log(
+    "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+  );
+
+  /* use events callback function to target the paragraph eleent with id="text" */
+  /* use innerHTML or textContent */
+
+//   function domHasLoadedText() {
+//     document.getElementById("text").innerHTML = "This is really cool!";
+//   }; 
