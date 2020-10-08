5fe938bc3302c219c8ebeae49e1896a92068f630
Shamel Lakin <christlife4u@gmail.com>
2020-10-08 17:52:16 -0400

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
-let theDomm = document.addEventListener("DOMContentLoaded", 
-function (){
-    console.log("This is really cool!");
-});
+let pTag = document.querySelector('p#text')
 
-console.log( "This console.log() fires when index.js loads - before DOMContentLoaded is triggered");
+document.addEventListener("DOMContentLoaded", changeParaTagText) 
+
+function changeParaTagText() {
+    pTag.innerHTML = "This is really cool!"
+}
+
+// const addXandY = () => {
+    //     let x = 5
+    //     let y = 6
+    //     p.innerHTML = x + y
+    // }
+    
+    //addXandY()
+
+//console.log( "This console.log() fires when index.js loads - before DOMContentLoaded is triggered");
 // addEventListener takes a String with the name 
 // of the event and a callback function.
